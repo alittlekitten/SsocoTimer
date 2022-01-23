d019f3948041cbb0b40e773f45369ce65f441b09
Ssoco <dlscjs8646@gmail.com>
2022-01-23 23:14:26 +0900

    ✨ 기본 설정 수정

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header.jsx";
+import Stopwatch from "./components/Stopwatch.jsx";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Stopwatch />
+    </>
+  );
+};
+
+export default App;
