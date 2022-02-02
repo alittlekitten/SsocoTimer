2f89b36616fc5803e5b5528e887f8eaf84688ddc
Ssoco <dlscjs8646@gmail.com>
2022-02-02 22:57:47 +0900

    🕷 Stopwatch 로직 변경으로 시간 안맞는 버그 수정
    setTimeout 기반에서 Date 기반으로 변경
    시간정밀도도 1/100에서 1/1000으로 증가

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -7,57 +7,69 @@ import { ReactComponent as Stop } from "../images/stop.svg";
 import { ReactComponent as Lap } from "../images/lap.svg";
 
 const Timer = () => {
+  const [ms, setMs] = useState(0); // 밀리초 (정밀도 1/1000)
   const [second, setSecond] = useState(0); // 초
   const [minute, setMinute] = useState(0); // 분
   const [hour, setHour] = useState(0); // 시
   const [lap, setLap] = useState([]); // 랩
   const [status, setStatus] = useState("stop");
   const playTimeout = useRef(null); // 재생버튼을 눌렀을 때 발생하는 Interval을 담기 위한 변수
+  const startTime = useRef(null);
+  const pauseTime = useRef(null);
 
   // 타이머 상태에 따른 동작
 
   const timePlay = () => {
     setStatus("play");
+    // 최초 시작
+    if (startTime.current === null) startTime.current = Date.now();
+    // pause후 시작
+    else startTime.current += Date.now() - pauseTime.current;
   };
 
   const timeLap = () => {
     if (status === "play")
-      setLap((prev) => [...prev, { hour, minute, second }]);
+      setLap((prev) => [...prev, { hour, minute, second, ms }]);
   };
 
   const timePause = () => {
     setStatus("pause");
     clearInterval(playTimeout.current);
+    pauseTime.current = Date.now();
   };
 
   const timeReset = () => {
     setStatus("stop");
-    setLap([]);
     clearInterval(playTimeout.current);
+    setLap([]);
     setSecond(0);
     setMinute(0);
     setHour(0);
+    setMs(0);
+    startTime.current = null;
+    pauseTime.current = null;
   };
 
   useEffect(() => {
     // play 눌렀을 때의 로직
+    const now = new Date(Date.now() - startTime.current);
+    console.log(
+      now.getUTCHours(),
+      now.getUTCMinutes(),
+      now.getUTCSeconds(),
+      now.getUTCMilliseconds()
+    );
     if (status === "play") {
       playTimeout.current = setTimeout(() => {
-        if (second === 59 && minute === 59) {
-          setHour(hour + 1);
-          setMinute(0);
-          setSecond(0);
-        } else if (second === 59) {
-          setMinute(minute + 1);
-          setSecond(0);
-        } else {
-          setSecond(second + 1);
-        }
-      }, 1000);
+        setHour(now.getUTCHours());
+        setMinute(now.getUTCMinutes());
+        setSecond(now.getUTCSeconds());
+        setMs(now.getUTCMilliseconds());
+      }, 1);
     }
-
+    // clean-up 함수의 실행 순서는 "state 업데이트 -> 리렌더링 -> 클린업 -> 새로운 이펙트 실행" 이기 때문에 useEffect의 동작에는 문제가 없다!
     return () => clearTimeout(playTimeout.current);
-  }, [hour, minute, second, status]);
+  }, [hour, minute, second, ms, status]);
 
   return (
     <div css={totalContainer}>
@@ -65,7 +77,8 @@ const Timer = () => {
         <p>
           {hour >= 10 ? hour : "0" + hour} :{" "}
           {minute >= 10 ? minute : "0" + minute} :{" "}
-          {second >= 10 ? second : "0" + second}
+          {second >= 10 ? second : "0" + second} :{" "}
+          {ms >= 100 ? ms : ms >= 10 ? "0" + ms : "00" + ms}
         </p>
       </div>
       <div css={playContainer({ status })}>
@@ -78,7 +91,14 @@ const Timer = () => {
         <h3>-- lap --</h3>
         {lap.map((elem, index) => (
           <p key={index}>
-            {elem.hour} : {elem.minute} : {elem.second}
+            {elem.hour >= 10 ? elem.hour : "0" + elem.hour} :{" "}
+            {elem.minute >= 10 ? elem.minute : "0" + elem.minute} :{" "}
+            {elem.second >= 10 ? elem.second : "0" + elem.second} :{" "}
+            {elem.ms >= 100
+              ? elem.ms
+              : elem.ms >= 10
+              ? "0" + elem.ms
+              : "00" + elem.ms}
           </p>
         ))}
       </div>
