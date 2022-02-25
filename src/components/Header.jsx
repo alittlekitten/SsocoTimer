02e49ccdac75bda8a9c8517a44526be7d1385f97
Ssoco <dlscjs8646@gmail.com>
2022-02-26 03:31:39 +0900

    📜 버전 정보 v1.1.1 업데이트

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,7 +20,7 @@ const Header = () => {
   return (
     <div>
       <div css={main}>
-        <span className="version">Ssoco Timer v1.1.0 </span>
+        <span className="version">Ssoco Timer v1.1.1 </span>
 
         <div className="icons">
           <span className="copyright">
