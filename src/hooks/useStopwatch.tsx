74ce025fff4ad326c89c2f73ac4514bcf2fad6b5
Ssoco <dlscjs8646@gmail.com>
2022-03-14 03:23:39 +0900

    ✨ Stopwatch 컴포넌트의 비즈니스 로직을 useStopwatch 커스텀 훅으로 분리

---
+++ src/hooks/useStopwatch.tsx
@@ -0,0 +1,82 @@
+import { useState, useRef, useEffect } from "react";
+import useTimeManage from "./utils/useTimeManage";
+
+interface LapState {
+  hour: number;
+  minute: number;
+  second: number;
+  ms: number;
+}
+
+const useStopwatch = () => {
+  const { time, setTime, playTimeout } = useTimeManage();
+  const { ms, second, minute, hour } = time;
+  const [lap, setLap] = useState<LapState[]>([]); // 랩
+  const [status, setStatus] = useState<string>("stop");
+  const startTime = useRef<number | null>(null); // 시작 시간을 담은 ref요소
+  const pauseTime = useRef<number | null>(null); // 잠시 멈춘 시간을 담은 ref요소
+
+  // 타이머 상태에 따른 동작
+
+  const timePlay = () => {
+    // 최초 시작
+    if (startTime.current === null && status !== "play")
+      startTime.current = Date.now();
+    // pause후 시작
+    else if (status === "pause")
+      startTime.current += Date.now() - pauseTime.current;
+    setStatus("play");
+  };
+
+  const timeLap = () => {
+    if (status === "play")
+      setLap((prev) => [...prev, { hour, minute, second, ms }]);
+  };
+
+  const timePause = () => {
+    if (status === "play") {
+      clearInterval(playTimeout.current);
+      pauseTime.current = Date.now();
+    }
+    setStatus("pause");
+  };
+
+  const timeReset = () => {
+    setStatus("stop");
+    clearInterval(playTimeout.current);
+    setLap([]);
+    setTime({ ...time, ms: 0, second: 0, minute: 0, hour: 0 });
+    startTime.current = null;
+    pauseTime.current = null;
+  };
+
+  useEffect(() => {
+    // play 눌렀을 때의 로직
+    const now = new Date(Date.now() - startTime.current);
+    if (status === "play") {
+      playTimeout.current = setTimeout(() => {
+        setTime({
+          ...time,
+          ms: now.getUTCMilliseconds(),
+          second: now.getUTCSeconds(),
+          minute: now.getUTCMinutes(),
+          hour: now.getUTCHours(),
+        });
+      }, 1);
+    }
+    // clean-up 함수의 실행 순서는 "state 업데이트 -> 리렌더링 -> 클린업 -> 새로운 이펙트 실행" 이기 때문에 useEffect의 동작에는 문제가 없다!
+    return () => clearTimeout(playTimeout.current);
+  }, [playTimeout, setTime, time, status]);
+
+  return {
+    time,
+    lap,
+    status,
+    timePlay,
+    timeLap,
+    timePause,
+    timeReset,
+  };
+};
+
+export default useStopwatch;
