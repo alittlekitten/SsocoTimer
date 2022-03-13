d798e144e63213b9e84d1c4c367a70a2048aa5cc
Ssoco <dlscjs8646@gmail.com>
2022-03-14 03:24:03 +0900

    ✨ Clock 컴포넌트의 비즈니스 로직을 useClock 커스텀 훅으로 분리

---
+++ src/hooks/useClock.tsx
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import useTimeManage from "./utils/useTimeManage";
+import { useSelector } from "react-redux";
+import { RootState } from "../store";
+
+const useClock = () => {
+  const { time, setTime, playTimeout } = useTimeManage();
+  const { hour12 } = useSelector((state: RootState) => state.clockReducer);
+
+  useEffect(() => {
+    const now = new Date(Date.now());
+    playTimeout.current = setTimeout(() => {
+      setTime({
+        ms: now.getMilliseconds(),
+        second: now.getSeconds(),
+        minute: now.getMinutes(),
+        hour: now.getHours(),
+        day: now.getDate(),
+        month: now.getMonth() + 1,
+        year: now.getFullYear(),
+      });
+    }, 1);
+
+    return () => clearTimeout(playTimeout.current);
+  }, [time, playTimeout, setTime]);
+
+  return { time, hour12 };
+};
+
+export default useClock;
