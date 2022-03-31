bb48e1f6c663a4cd76419f4581a2eb2110850b45
Ssoco <dlscjs8646@gmail.com>
2022-04-01 08:32:11 +0900

    ✨ emotion의 themeProvider를 이용하여 모든 컴포넌트에서 변수로 지정한 색깔값 사용
    theme으로 전달해준 요소는 해당 컴포넌트에서는 사용할 수 없기에 App에서 해당 요소를 쓰기 위해 ColorSet에 직접 접근해서 사용하는 방식으로 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react";
+import { css, ThemeProvider } from "@emotion/react";
 import { useSelector } from "react-redux";
 import Title from "@components/Title";
 import Nav from "@components/Nav";
@@ -12,9 +12,13 @@ import useTimer from "@hooks/useTimer";
 import "@src/App.css";
 import useStopwatch from "./hooks/useStopwatch";
 import useClock from "./hooks/useClock";
+import colorSet from "./styles/palette";
 
 const App = () => {
   const { tap } = useSelector((state: RootState) => state.tapReducer); // tapReducer의 상태를 가져올 수 있다. (여기서는 구조분해할당으로 가져옴)
+  const { themeStatus } = useSelector(
+    (state: RootState) => state.optionReducer
+  );
   // 리렌더링을 줄이기 위해서는 최대한 자세하게 가져오는 것이 좋음
 
   const timerProps = useTimer();
@@ -22,18 +26,35 @@ const App = () => {
   const clockProps = useClock();
 
   return (
-    <div css={PageContainer}>
-      <Nav />
-      <div css={MainContainer}>
-        <Header />
-        <Title />
-        {tap === "Timer" && <Timer props={timerProps} />}
-        {tap === "Stopwatch" && <Stopwatch props={stopwatchProps} />}
-        {tap === "Clock" && <Clock props={clockProps} />}
+    <ThemeProvider
+      theme={themeStatus == "dark" ? colorSet.dark : colorSet.light}
+    >
+      <div css={Viewport(themeStatus)}>
+        <div css={PageContainer}>
+          <Nav />
+          <div css={MainContainer}>
+            <Header />
+            <Title />
+            {tap === "Timer" && <Timer props={timerProps} />}
+            {tap === "Stopwatch" && <Stopwatch props={stopwatchProps} />}
+            {tap === "Clock" && <Clock props={clockProps} />}
+          </div>
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 };
+
+const Viewport = (themeStatus: string) => css`
+  width: 100vw;
+  height: 100vh;
+  background-color: ${themeStatus === "light"
+    ? colorSet.light.bgPage1
+    : colorSet.dark.bgPage1};
+  color: ${themeStatus === "light"
+    ? colorSet.light.text1
+    : colorSet.dark.text1};
+`;
 
 const PageContainer = css`
   position: absolute;
